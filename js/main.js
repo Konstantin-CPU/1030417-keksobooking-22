57377ceb7d80f0ba84cbaa7d7a092c81777634ed
Константин <konstantingoncharuk50@gmail.com>
2021-01-31 16:40:36 +0300

    Нужно больше функций

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+
+const getRandomNumber = function (min, max) {
+  if (min < 0 || max <= min) {
+    return 'Перепроверьте введенные значения!';
+  }
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const getRandomNumberWithPoint = function (min, max, numbersAfterPoint) {
+  if (min < 0 || max <= min) {
+    return 'Перепроверьте введенные значения!';
+  }
+  return (Math.random() * (max - min + 1) + min).toFixed(numbersAfterPoint);
+}
+
+alert(getRandomNumber(0, 1000));
+alert(getRandomNumberWithPoint(0, 1000, 4));
