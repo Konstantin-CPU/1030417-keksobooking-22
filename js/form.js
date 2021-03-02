01ed1b1deb37a1a002dc30d211cad6b9bf466af9
Константин <konstantingoncharuk50@gmail.com>
2021-03-02 13:53:23 +0300

    Исправил синхронизацию полей

--- js/form.js
+++ js/form.js
@@ -31,128 +31,147 @@ const getFormAccessibility = (boolean) => {
 
 getFormAccessibility();
 
-const TITLE_MIN_LENGTH = 30;
-const TITLE_MAX_LENGTH = 100;
-
-const title = document.querySelector('#title');
-title.addEventListener('input', () => {
-  const valueLength = title.value.length;
-  if (valueLength < TITLE_MIN_LENGTH) {
-    title.setCustomValidity(`Ещё ${(TITLE_MIN_LENGTH - valueLength)} симв.`);
-  } else if (valueLength > TITLE_MAX_LENGTH) {
-    title.setCustomValidity(`Уберите ${(valueLength - TITLE_MAX_LENGTH)} симв.`)
-  } else {
-    title.setCustomValidity('');
-  }
-
-  title.reportValidity();
-});
-
 const addressInput = document.querySelector('#address');
-addressInput.value = '35.6894, 139.692';
 
-const selectTypeForm = document.querySelector('#type');
-const priceInput = document.querySelector('#price');
-const timeInForm = document.querySelector('#timein');
-const timeOutForm = document.querySelector('#timeout');
+window.addEventListener('load', () => {
+  const TITLE_MIN_LENGTH = 30;
+  const TITLE_MAX_LENGTH = 100;
 
-const minPrice = offerData.minPrice;
-
-const setMinPrice = () =>  {
-  const minPriceKeys = Object.keys(minPrice);
-  minPriceKeys.forEach(element => {
-    if (element === selectTypeForm.value) {
-      priceInput.placeholder = minPrice[element];
-      priceInput.min = minPrice[element];
+  const title = document.querySelector('#title');
+  title.addEventListener('input', () => {
+    const valueLength = title.value.length;
+    if (valueLength < TITLE_MIN_LENGTH) {
+      title.setCustomValidity(`Ещё ${(TITLE_MIN_LENGTH - valueLength)} симв.`);
+    } else if (valueLength > TITLE_MAX_LENGTH) {
+      title.setCustomValidity(`Уберите ${(valueLength - TITLE_MAX_LENGTH)} симв.`)
+    } else {
+      title.setCustomValidity('');
     }
+
+    title.reportValidity();
   });
-};
 
-selectTypeForm.addEventListener('change', setMinPrice);
 
-const PRICE_MAX = 1000000;
+  addressInput.value = '35.6894, 139.692';
 
-priceInput.addEventListener('input', () => {
-  if (priceInput.value > PRICE_MAX) {
-    priceInput.setCustomValidity(`Максимальная цена - ${(PRICE_MAX)} руб.`)
-  } else {
-    priceInput.setCustomValidity('');
+  const selectTypeForm = document.querySelector('#type');
+  const priceInput = document.querySelector('#price');
+  const timeInForm = document.querySelector('#timein');
+  const timeOutForm = document.querySelector('#timeout');
+
+  const minPrice = offerData.minPrice;
+
+  const setMinPrice = () =>  {
+    const minPriceKeys = Object.keys(minPrice);
+    minPriceKeys.forEach(element => {
+      if (element === selectTypeForm.value) {
+        priceInput.placeholder = minPrice[element];
+        priceInput.min = minPrice[element];
+      }
+    });
+  };
+
+  selectTypeForm.addEventListener('change', setMinPrice);
+
+  const PRICE_MAX = 1000000;
+
+  priceInput.addEventListener('input', () => {
+    if (priceInput.value > PRICE_MAX) {
+      priceInput.setCustomValidity(`Максимальная цена - ${(PRICE_MAX)} руб.`)
+    } else {
+      priceInput.setCustomValidity('');
+    }
+
+    priceInput.reportValidity();
+  })
+
+  let changeEvent = new Event('change');
+  selectTypeForm.dispatchEvent(changeEvent);
+
+  timeInForm.addEventListener('change', function(evt) {
+    timeOutForm.value = evt.target.value;
+  });
+
+  timeOutForm.addEventListener('change', function(evt) {
+    timeInForm.value = evt.target.value;
+  });
+
+  const roomSelect = document.querySelector('#room_number');
+  const guestsSelect = document.querySelector('#capacity');
+
+  for (let i = 0; i < roomSelect.length; i++) {
+    if (guestsSelect[i].value !== '1') {
+      guestsSelect[i].disabled;
+    }
   }
 
-  priceInput.reportValidity();
+
+
+  const getAvailableOptions = (value, selectedInput, processingInput) => {
+    for (let i = 0; i < processingInput.length; i++) {
+      let processingElement = processingInput[i];
+      let processingValue = processingElement.value;
+
+      processingElement.disabled = false;
+
+      if (selectedInput.value < '2') {
+        if (processingValue !== `${value}`) {
+          processingElement.disabled = true;
+        }
+      }
+      if (selectedInput === roomSelect) {
+        const exeption = !(processingValue > '0' && processingValue < '3');
+        const secondExeption = !(processingValue > '0' && processingValue < '4');
+
+        if (selectedInput.value === '2') {
+          if (exeption) {
+            processingElement.disabled = true;
+          }
+        }
+        if (selectedInput.value === '3') {
+          if (secondExeption) {
+            processingElement.disabled = true;
+          }
+        }
+      }
+
+      if (selectedInput === guestsSelect) {
+        const exeption = selectedInput.value > '0' && selectedInput.value < '3';
+        const secondExeption = selectedInput.value > '0' && selectedInput.value < '4';
+
+        if (exeption) {
+          if (!(processingValue > '0' && processingValue < '3')) {
+            processingElement.disabled = true;
+          }
+        }
+        if (secondExeption) {
+          if (!(processingValue > '0' && processingValue < '4')) {
+            processingElement.disabled = true;
+          }
+        }
+      }
+    }
+  }
+
+  getAvailableOptions(parseInt(roomSelect.value), roomSelect, guestsSelect);
+
+
+  const controlGuestSelectOption = () => {
+    const roomValue = parseInt(roomSelect.value);
+    getAvailableOptions(roomValue, roomSelect, guestsSelect);
+  }
+
+  const controlRoomSelectOption = () => {
+    const guestValue = parseInt(guestsSelect.value);
+    getAvailableOptions(guestValue, guestsSelect, roomSelect);
+  }
+
+  roomSelect.addEventListener('change', controlGuestSelectOption);
+  guestsSelect.addEventListener('change', controlRoomSelectOption);
 })
 
-let changeEvent = new Event('change');
-selectTypeForm.dispatchEvent(changeEvent);
 
-timeInForm.addEventListener('change', function(evt) {
-  timeOutForm.value = evt.target.value;
-});
 
-timeOutForm.addEventListener('change', function(evt) {
-  timeInForm.value = evt.target.value;
-});
 
-const roomSelect = document.querySelector('#room_number');
-const guestsSelect = document.querySelector('#capacity');
-
-const getAvailableOptions = (value, selectedInput, processingInput) => {
-  for (let i = 0; i < processingInput.length; i++) {
-    let processingElement = processingInput[i];
-    let processingValue = processingElement.value;
-
-    processingElement.disabled = false;
-
-    if (selectedInput.value < '2') {
-      if (processingValue !== `${value}`) {
-        processingElement.disabled = true;
-      }
-    }
-    if (selectedInput === roomSelect) {
-      const exeption = !(processingValue > '0' && processingValue < '3');
-      const secondExeption = !(processingValue > '0' && processingValue < '4');
-
-      if (selectedInput.value === '2') {
-        if (exeption) {
-          processingElement.disabled = true;
-        }
-      }
-      if (selectedInput.value === '3') {
-        if (secondExeption) {
-          processingElement.disabled = true;
-        }
-      }
-    }
-
-    if (selectedInput === guestsSelect) {
-      const exeption = selectedInput.value > '0' && selectedInput.value < '3';
-      const secondExeption = selectedInput.value > '0' && selectedInput.value < '4';
-
-      if (exeption) {
-        if (!(processingValue > '0' && processingValue < '3')) {
-          processingElement.disabled = true;
-        }
-      }
-      if (secondExeption) {
-        if (!(processingValue > '0' && processingValue < '4')) {
-          processingElement.disabled = true;
-        }
-      }
-    }
-  }
-}
-
-const controlGuestSelectOption = () => {
-  const roomValue = parseInt(roomSelect.value);
-  getAvailableOptions(roomValue, roomSelect, guestsSelect);
-}
-
-const controlRoomSelectOption = () => {
-  const guestValue = parseInt(guestsSelect.value);
-  getAvailableOptions(guestValue, guestsSelect, roomSelect);
-}
-
-roomSelect.addEventListener('change', controlGuestSelectOption);
-guestsSelect.addEventListener('change', controlRoomSelectOption);
 
 export {getFormAccessibility, addressInput, offersArray};
